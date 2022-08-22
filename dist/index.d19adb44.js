7e80fbfbc20eca509367ed3e710ea23c56de8166
khaitbek <khaitbek2005@gmail.com>
2022-08-22 06:52:30 +0500

    Finish footer

---
+++ dist/index.d19adb44.js
@@ -0,0 +1,2 @@
+const headerOuter=document.querySelector(".header__outer"),hamburger=document.querySelector(".hamburger");hamburger.addEventListener("click",(()=>{hamburger.toggleAttribute("toggleNav"),headerOuter.classList.toggle("show")}));
+//# sourceMappingURL=index.d19adb44.js.map
