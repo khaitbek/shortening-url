7e5ff18f50b438fd56de5457b60500451f900564
khaitbek <khaitbek2005@gmail.com>
2022-10-16 23:57:54 +0500

    connect to API

---
+++ dist/index.aee44547.js
@@ -0,0 +1,23 @@
+const userForm = document.querySelector("#userForm");
+const userInput = userForm.querySelector("#userInput");
+const userShortenedLink = userForm.querySelector("#userShortenedLink");
+userForm.addEventListener("submit", (evt)=>{
+    evt.preventDefault();
+    const userLink = userInput.value;
+    const sendData = JSON.stringify({
+        "link": userLink
+    });
+    fetch("http://ec2-52-42-75-134.us-west-2.compute.amazonaws.com:8080/url", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: sendData
+    }).then((res)=>res.json()).then((data)=>{
+        userShortenedLink.style.display = "block";
+        userShortenedLink.textContent = `Your shortened link is ${data.url}`;
+        console.log(data);
+    });
+});
+
+//# sourceMappingURL=index.aee44547.js.map
